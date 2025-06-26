a20d4e54c11210f371c993c5a953394809c9b23b
Florian devusSs Försterling <flofest98@gmail.com>
2025-06-26 15:33:11 +0200

    refactored projects file in cmd package and added delete command for a tracking project

--- cmd/projects.go
+++ cmd/projects.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -10,31 +11,39 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	baseDir        string
+	loadedProjects []*projects.Project
+)
+
 var projectsCmd = &cobra.Command{
 	Use:   "projects",
 	Short: "Manage and list tracking projects",
+	PersistentPreRun: func(_ *cobra.Command, _ []string) {
+		homeDir, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		baseDir = filepath.Join(homeDir, ".dropawp")
+
+		loadedProjects, err = projects.Load(baseDir)
+		cobra.CheckErr(err)
+	},
 }
+
+const listProjectsTabPadding = 2
 
 var listProjectsCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all tracking projects",
 	Run: func(_ *cobra.Command, _ []string) {
-		baseDir, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		baseDir = filepath.Join(baseDir, ".dropawp")
-
-		projects, err := projects.Load(baseDir)
-		cobra.CheckErr(err)
-
-		if len(projects) == 0 {
+		if len(loadedProjects) == 0 {
 			fmt.Println("No tracking projects found.")
 			fmt.Println("Run 'dropawp new' to create a new project.")
 			return
 		}
 
 		if listProjectsJSON {
-			for _, project := range projects {
+			for _, project := range loadedProjects {
 				fmt.Println(project.JSON())
 				fmt.Println()
 			}
@@ -42,17 +51,17 @@ var listProjectsCmd = &cobra.Command{
 		}
 
 		if listProjectsText {
-			for _, project := range projects {
+			for _, project := range loadedProjects {
 				fmt.Println(project.String())
 				fmt.Println()
 			}
 			return
 		}
 
-		w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, listProjectsTabPadding, ' ', 0)
 		fmt.Fprintln(w, "NAME\tLOGS DIR\tCONFIG FILE\tSTORAGE DIR")
 
-		for _, project := range projects {
+		for _, project := range loadedProjects {
 			fmt.Fprintf(
 				w,
 				"%s\t%s\t%s\t%s\n",
@@ -63,8 +72,37 @@ var listProjectsCmd = &cobra.Command{
 			)
 		}
 
-		err = w.Flush()
+		err := w.Flush()
 		cobra.CheckErr(err)
+	},
+}
+
+var deleteProjectCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete a tracking project by its name",
+	Run: func(_ *cobra.Command, args []string) {
+		if deleteProjectName == "" {
+			cobra.CheckErr(errors.New("Project name is required"))
+		}
+
+		newProjects := make([]*projects.Project, 0)
+		appended := 0
+
+		for _, project := range loadedProjects {
+			if project.Name != deleteProjectName {
+				newProjects = append(newProjects, project)
+				appended++
+			}
+		}
+
+		if appended == len(loadedProjects) {
+			cobra.CheckErr(fmt.Errorf("Project '%s' not found", deleteProjectName))
+		}
+
+		err := projects.SaveProjects(baseDir, newProjects)
+		cobra.CheckErr(err)
+
+		fmt.Printf("Project '%s' deleted successfully.\n", deleteProjectName)
 	},
 }
 
@@ -72,6 +110,8 @@ var (
 	listProjectsJSON bool
 	listProjectsText bool
 )
+
+var deleteProjectName string
 
 func init() {
 	rootCmd.AddCommand(projectsCmd)
@@ -82,4 +122,9 @@ func init() {
 		BoolVarP(&listProjectsJSON, "json", "j", false, "Output projects information in JSON format")
 	listProjectsCmd.Flags().
 		BoolVarP(&listProjectsText, "text", "t", false, "Output projects information in text format")
+
+	projectsCmd.AddCommand(deleteProjectCmd)
+	deleteProjectCmd.Flags().
+		StringVarP(&deleteProjectName, "name", "n", "", "Name of the project to delete")
+	deleteProjectCmd.MarkFlagRequired("name")
 }

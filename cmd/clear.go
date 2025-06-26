4f983526e541e1714faffb8b6e377df2d3b95cf7
Florian devusSs Försterling <flofest98@gmail.com>
2025-06-27 00:09:25 +0200

    added clear command to remove all application data in a quick and efficient way

---
+++ cmd/clear.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/devusSs/dropawp/internal/stdin"
+	"github.com/spf13/cobra"
+)
+
+var clearCmd = &cobra.Command{
+	Use:   "clear",
+	Short: "Clears all data saved by the application",
+	Run: func(cmd *cobra.Command, args []string) {
+		confirm, err := stdin.ReadFromStdin(
+			"Are you sure you want to clear all data? This action cannot be undone (yes/no)",
+		)
+		cobra.CheckErr(err)
+
+		if confirm != "yes" {
+			fmt.Println("Cancelled, no data was cleared.")
+			return
+		}
+
+		homeDir, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		baseDir := filepath.Join(homeDir, ".dropawp")
+
+		err = os.RemoveAll(baseDir)
+		cobra.CheckErr(err)
+
+		fmt.Println("All data cleared successfully.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(clearCmd)
+}

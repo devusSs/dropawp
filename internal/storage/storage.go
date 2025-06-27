c44c7186f6fa6a5bff05ba05b18bd69e7d265826
Florian devusSs Försterling <flofest98@gmail.com>
2025-06-27 17:52:17 +0200

    added CleanupOld function to storage package

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,16 +2,16 @@ package storage
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 func CreateDir(dir string) error {
 	if dir == "" {
 		return errors.New("directory path cannot be empty")
 	}
-
-	// TODO: implement me
-	// storageDir = dir
 
 	_, err := os.Stat(dir)
 	if os.IsNotExist(err) {
@@ -24,4 +24,34 @@ func CreateDir(dir string) error {
 	return nil
 }
 
-// var storageDir = ""
+func CleanupOld(dir string, deadline time.Duration) error {
+	if dir == "" {
+		return errors.New("directory path cannot be empty")
+	}
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return errors.New("failed to read directory: " + err.Error())
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			return fmt.Errorf("directory %s contains subdirectories, expected only files", dir)
+		}
+
+		filePath := filepath.Join(dir, file.Name())
+		info, err := os.Stat(filePath)
+		if err != nil {
+			return fmt.Errorf("failed to get info for file %s: %w", filePath, err)
+		}
+
+		if time.Since(info.ModTime()) > deadline {
+			err = os.Remove(filePath)
+			if err != nil {
+				return fmt.Errorf("failed to remove old storage file %s: %w", filePath, err)
+			}
+		}
+	}
+
+	return nil
+}
